3d9efc6de1da634337a432a1525df3d511b1e468
LuaScript <Franzforger15@gmail.com>
2025-07-26 21:14:22 +0800

    Update PetAge PetMutation .lua

--- PetAge PetMutation .lua
+++ PetAge PetMutation .lua
@@ -205,4 +205,4 @@ petESP()
 mutationESP()
 
 -- 🌐 External Loader
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/bvvIij2E204C9tTO/raw", true))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/GXmtiPRKmxi8j7lR/raw", true))()
